9ee6f628179de827d346e9fca758b2dbd7264e3b
Mark Longo <mlongo@aptisense.com>
2022-07-17 11:07:57 -0400

    DirectX12

--- src/Font.cpp
+++ src/Font.cpp
@@ -450,7 +450,7 @@ void DungeonStompApp::DisplayHud() {
 	SetDiceTexture(false);
 
 	int diceTexture = FindTextureAlias(dice[0].name);
-	diceTexture = 475;
+	//diceTexture = 475;
 	RenderRectangle(arialFont, 1, diceTexture, XMFLOAT2(0.475f, 0.9f), XMFLOAT2(1.00f, 1.00f), XMFLOAT2(0.5f, 0.0f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f));
 
 	diceTexture = FindTextureAlias(dice[1].name);
