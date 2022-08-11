6552c9edd74617d923a11c3662e0dc08ba577b72
Mark Longo <mlongo@aptisense.com>
2022-08-11 15:35:10 -0400

    DirectX12 Screen space ambient occlusion (ssao)

--- Shaders/Shadows.hlsl
+++ Shaders/Shadows.hlsl
@@ -56,12 +56,12 @@ void PS(VertexOut pin)
 	// Dynamically look up the texture in the array.
 	diffuseAlbedo *= gDiffuseMap.Sample(gsamAnisotropicWrap, pin.TexC);
 
-#ifdef ALPHA_TEST
+//#ifdef ALPHA_TEST
     // Discard pixel if texture alpha < 0.1.  We do this test as soon 
     // as possible in the shader so that we can potentially exit the
     // shader early, thereby skipping the rest of the shader code.
     clip(diffuseAlbedo.a - 0.1f);
-#endif
+//#endif
 }
 
 
