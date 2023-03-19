5332ab7323cb427e0e5fb4bbed66893b7c35435a
Mark Longo <mlongo@aptisense.com>
2023-03-18 23:43:48 -0400

    DirectX12

--- src/DungeonStompApp.cpp
+++ src/DungeonStompApp.cpp
@@ -281,6 +281,8 @@ void DungeonStompApp::Draw(const GameTimer& gt)
 	// Bind null SRV for shadow map pass.
 	mCommandList->SetGraphicsRootDescriptorTable(5, mNullSrv);
 
+	ScanMod(gt.DeltaTime());
+
 	//Render shadow map to texture.
 	DrawSceneToShadowMap(gt);
 
@@ -2209,7 +2211,6 @@ void DungeonStompApp::DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const 
 		DisplayHud();
 		SetDungeonText();
 	}
-	ScanMod(gt.DeltaTime());
 
 	return;
 }
